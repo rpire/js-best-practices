9f8a2ddd4d9ef82f7dfc9491fd444163f96e6147
rpire <rubenpire7@gmail.com>
2021-08-23 11:12:15 -0400

    Repositroy set-up

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# js-best-practices
-An exercise of the third week on the second module of the Microverse Program
+# To-Do List
+
+> This is a project for the second week from module: #2 of the Microverse Program.
+
+## Built With
+
+- HTML
+- CSS
+- JS
+
+## Live Demo
+[Live Demo](https://rpire.github.io/to-do-list/dist/index.html)
+
+## Getting Started
+
+To get started locally follow these simple example steps.
+- Fork and clone the repo to your computer using `git clone https://github.com/rpire/to-do-list.git` in the terminal or command prompt.
+- Make sure to `git checkout desired-branch`.
+- Go to the `dist` folder and open `index.html` to see the project in your browser, or just run the Live Demo with the link above.
+
+## Author
+
+👤 **Rubén D. Pire**
+
+- GitHub: [Rubén D. Pire](https://github.com/rpire)
+- LinkedIn: [Rubén D. Pire](https://www.linkedin.com/in/rub%C3%A9n-dar%C3%ADo-pire-l%C3%B3pez-507111189/)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
---
+++ src/index.js
@@ -0,0 +1,69 @@
+import genHTML from './gen_html';
+import addNew from './add_new';
+import './style.css';
+
+let listArr = [];
+
+const list = document.getElementById('list');
+
+const reload = () => {
+  if (localStorage.getItem('toDoList')) {
+    const oldStorage = localStorage.getItem('toDoList');
+    const newStorage = JSON.parse(oldStorage);
+    list.innerHTML = '';
+    listArr = newStorage;
+    genHTML(list, listArr);
+  } else {
+    const defaultItem = [
+      {
+        description: 'Add your first task',
+        completed: false,
+        index: 1,
+      },
+    ];
+    const newStorage = JSON.stringify(defaultItem);
+    listArr = defaultItem;
+    localStorage.setItem('toDoList', newStorage);
+    list.innerHTML = '';
+    genHTML(list, JSON.parse(newStorage));
+  }
+  const checkboxes = document.querySelectorAll('.checkbox');
+  for (let i = 0; i < checkboxes.length; i += 1) {
+    checkboxes[i].addEventListener('change', () => {
+      reload();
+    });
+  }
+};
+
+window.onload = reload();
+
+const task = document.getElementById('add-item');
+const addNewBtn = document.getElementById('add-new-btn');
+addNewBtn.addEventListener('click', () => {
+  if (task.value.length > 0) {
+    listArr.push(addNew(task.value, listArr.length + 1));
+    const newStorage = JSON.stringify(listArr);
+    localStorage.setItem('toDoList', newStorage);
+    task.value = '';
+    reload();
+  } else {
+    alert('Tasks must contain text.\nTry typing something into the "Add to your list..." input.');
+  }
+});
+
+const clrBtn = document.getElementById('clear-all');
+clrBtn.addEventListener('click', () => {
+  for (let i = 0; i < listArr.length; i += 1) {
+    if (listArr[i].completed) {
+      listArr.splice(i, 1);
+      listArr.forEach((task) => {
+        if (task.index > i) {
+          task.index -= 1;
+        }
+      });
+      i -= 1;
+    }
+  }
+  localStorage.setItem('toDoList', JSON.stringify(listArr));
+  reload();
+});
